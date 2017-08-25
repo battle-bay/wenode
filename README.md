0f4173e7bec2c1bb28d7158dc9c886caaff8f437
marlinl <malin3369@gmail.com>
2017-08-25 18:39:27 +0800

    update read me and modify entry point

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+<a href="https://travis-ci.org/battle-bay/wenode"><img align="right" src="https://travis-ci.org/battle-bay/wenode.svg?branch=master" alt="Build status" /></a>
 ## The wechat offical account manager service
--- app.js
+++ app.js
@@ -1,6 +1,17 @@
+// The web framework using express
+// auther: marlinl
+// created at: Fri Aug 25 2017
+
 const express = require('express');
+const config  = require('./config');
+
+var officalLocals = require('./core/middleware/wechat-general-locals');
+
+
+
 const app     = express();
 
+app.use('/wechat/api/v0/', officalLocals);
 
 app.listen(3000, function () {
   console.log('Server has listening 3000');
